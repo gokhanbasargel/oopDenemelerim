4074a79dd775a725af322ec4b32ab563a2cd79c2
gokhanBasargel <gokhan.basergel@gmail.com>
2022-09-14 18:09:11 +0300

    Initial commit

---
+++ src/Product.java
@@ -0,0 +1,69 @@
+public class Product {
+    public Product() {
+        System.out.println("Ben çalıştım");
+    }
+    public Product(int id,String name,double unitPrice, String detail,double discount) {
+        this();
+        this.setId(id);
+        this.setName(name);
+        this.setUnitPrice(unitPrice);
+        this.setDetail(detail);
+        this.setDiscount(discount);
+
+
+
+    }
+    private int id;
+    private String name;
+    private double unitPrice;
+    private String detail;
+    private double discount;
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+
+    public double getUnitPriceAfterDiscount() {
+        return this.unitPrice-(this.discount * this.unitPrice / 100);
+    }
+
+    }
+
